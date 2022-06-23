5d271b8975d597522b2fde55fcdbf5fb37650b4e
alejandroserrano <ajserrano9310@gmail.com>
2022-06-23 17:56:10 -0600

    Created new componenent

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Test } from "./components/Test"; 
 
 const AppRoutes = [
   {
@@ -14,7 +15,11 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
-  }
+  },
+    {
+        path: '/test',
+        element: <Test />
+    }
 ];
 
 export default AppRoutes;
