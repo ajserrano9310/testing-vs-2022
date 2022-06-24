dd8ed317fb6abbcabe3057c78e5af8611dfbe005
alejandroserrano <ajserrano9310@gmail.com>
2022-06-23 19:46:44 -0600

    Adding test file

---
+++ ClientApp/src/components/Test.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class Test extends Component {
+    render() {
+        return (
+            <div>
+                <h1> This is the test component</h1>
+            </div>
+        );
+    }
+}
+
